04ab1b4519d03544a3758a2becb139b2b5fb4cdd
pshakhane <100415565+pshakhane@users.noreply.github.com>
2024-09-15 14:01:34 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,5 @@
-$(function() {
-
-  var $newItemButton = $('#newItemButton');
-  var $newItemForm = $('#newItemForm');
-  var $textInput = $('input:text');
-
-  $newItemButton.show();
-  $newItemForm.hide();
-
-  $('#showForm').on('click', function(){
-    $newItemButton.hide();
-    $newItemForm.show();
-  });
-
-  $newItemForm.on('submit', function(e){
-    e.preventDefault();
-    var newText = $textInput.val();
-    $('label:last').after('<label>' + newText + '</label>');
-    $newItemForm.hide();
-    $newItemButton.show();
-    $textInput.val('');
-  });
-
+document.getElementById('inquiryForm').addEventListener('submit', function(e) {
+    e.preventDefault(); // Prevent form submission
+    alert('Your inquiry has been submitted. We will contact you soon!');
 });
+
